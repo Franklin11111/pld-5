db38825538b9f4231ae4716735a5a12b0e346deb
Franklin11111 <abdulovnail6@gmail.com>
2025-07-09 19:40:24 +0400

    Update app.js file by creating validateForm() function that checks if the input fields are empty or textarea is too short

--- app.js
+++ app.js
@@ -3,24 +3,36 @@ const navMenu = document.querySelector(".nav-menu");
 const form = document.querySelector("form");
 const email = document.getElementById("email");
 const nameInput = document.getElementById("name");
+const messageInput = document.getElementById("message");
+const errorName = document.querySelector(".error-name");
+const errorEmail = document.querySelector(".error-email");
+const errorMessage = document.querySelector(".error-message");
 
 menuIcon.addEventListener('click', () => {
     navMenu.classList.toggle("display-nav")
 })
 
-
-email.addEventListener("input", (event) => {
-    event.preventDefault();
-    if (email.validity.typeMismatch) {
-        email.setCustomValidity("I am expecting an email address!");
-    } else {
-        email.setCustomValidity("");
+function validateForm() {
+    let nameValue = nameInput.value;
+    let emailValue = email.value;
+    let messageValue = messageInput.value;
+    if (nameValue == "") {
+        errorName.innerHTML = "Name field is empty."
+        alert("Name must be filled out");
+        return false;
     }
-});
-
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    if (!email.validity.typeMismatch || email.validity.valueMissing) {
-        alert("data is correct");
+    else if (emailValue == "") {
+        errorEmail.innerHTML = "Email field is empty."
+        alert("Email must be filled out");
+        return false;
     }
-})
+    else if (messageValue.length < 10) {
+        errorMessage.innerHTML = "Message field is not long enough to be submitted."
+        alert("Message is too short");
+        return false;
+    }
+    else {
+        alert("Thanks! You data was submitted successfully.")
+        return true;
+    }
+}
